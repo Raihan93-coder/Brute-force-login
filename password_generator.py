780bf85480df36bc24283d5f77751783f3d5f353
Raihan93-coder <raihanshamnad93@gmail.com>
2025-05-05 20:59:01 +0530

    Add files via upload

---
+++ password_generator.py
@@ -0,0 +1,38 @@
+#This program is to generate a minimum length password that is 8 character using name and date of birth
+#The obtained password will be stored in a txt file named as wordlist.txt and this will be word list used for initiating the attack
+
+import random
+import os
+import time
+
+#function using to generate random password from the given input
+def random_password(name,dob,length):
+    new = name + dob
+    temp = list(new)
+    passwords = []
+
+    random.seed(time.time())#always gives out unique password
+    for i in range(100):
+        random.shuffle(temp)
+        passwords.append(''.join(temp[:length]))
+    generate_wordlist(passwords)
+
+#saves the generated password inside a txt file from a list
+def generate_wordlist(arr):
+
+    if os.path.exists("wordlist.txt"):
+        #checking if wordlist.txt file exsist if it does opening in append mode
+        f = open("wordlist.txt","a")
+        for i in range(100):
+            f.write(str(arr[i]) + "\n")
+        f.close()
+    else:
+        f = open("wordlist.txt","w")
+        for i in range(100):
+            f.write(str(arr[i]) + "\n")
+        f.close()
+
+name = input()
+date = input()
+length = int(input())
+random_password(name,date,length)
